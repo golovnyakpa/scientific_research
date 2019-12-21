e7ade24fd984f59c73772a3c53e616a8c41f416a
golovnyakpa <pgolovnyak@gmail.com>
2019-12-21 13:53:05 +0300

    changed way of time measurment

--- nir/get_assignment_time.c
+++ nir/get_assignment_time.c
@@ -2,34 +2,30 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h> 
+#include <stdio.h>
+#include <unistd.h>
 
-static double diffclock(clock_t clock1,clock_t clock2)
-{
-    double diffticks=clock1-clock2;
-    double diffms=(diffticks)/(CLOCKS_PER_SEC/1000);
-    return diffms;
-}
+
+typedef unsigned long long ull;
+
+
+ull rdtsc() 
+	{
+		unsigned int lo, hi;
+		asm volatile ( "rdtsc\n" : "=a" (lo), "=d" (hi) );
+		return ((ull)hi << 32) | lo;
+    }
+
 
 int main()
 {
-	int i, j, a, b = 7, c = 9;
-	clock_t start, end;
-	double results[100], res, answer, sum = 0;
-	for (i=0; i<100; i++)
-	{
-		start =clock();
-		for (j=0; j<100000; j++)
-		{
-		    a = b*c;
-		}
-		end = clock();
-	    res = diffclock(end, start);
-	    results[i] = res;
-    }
-    for (i=0; i<100; i++)
-        sum += results[i];
-    answer = sum / 100;
-	printf("Алгоритм работал %f\n", answer);
+	int a, i;
+	float time;
+	ull t1 = rdtsc();
+	for (i=0; i < 1000; i++)
+	    a = 3 ^ 5;
+	ull t2 = rdtsc();
+	time = (float)((t2 - t1)/(CLOCKS_PER_SEC/1000));
+	printf("%f\n", time / 1000);
 	return 0;
 }
